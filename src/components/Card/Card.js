093f172d2c3d4f10e2b0e8598e6e0c967b29f532
waramos97 <waramos97@gmail.com>
2024-04-15 01:54:35 -0300

    Instalação do prime react e ajuste nos card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,46 +1,42 @@
 import React from 'react'
-import {tipos,setColor,capitalize} from'./cardController'
+import { tipos, setColor, capitalize } from './cardController'
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+
 import './styles.css'
 
 
 
-const RenderCard = (pokemon,index)=>{
+const RenderCard = (pokemon, index) => {
     const types = tipos(pokemon.types)
 
-     return( 
-        <div className='cardPersonal' key={index}>
-        <img alt='Pokemon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`}/>
-        
-            <h4>{capitalize(pokemon.name)}</h4>
-            {
-                types.length===1?
-                <div>
-                    <h2 className='badge ' style={setColor(types[0])}>{types[0]}</h2>
-                    </div>:
-                 <div>
-                    <h6 className='badge' style={setColor(types[0])}>{types[0]}</h6>
-                    <h6 className='badge ml-1' style={setColor(types[1])}>{types[1]}</h6>
-                </div>
-            }
-        
-        <button className='btn btn-danger'> More</button>
-    </div>
-    ) 
+    const header = (
+        <img alt='Pokemon' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} />
+    );
+
+    const footer = (
+        <Button className='btn btn-danger' style={{ marginLeft: '0.5em' }} label="Check" icon="pi pi-check" />
+    );
+
+    return (
+        <div>
+            <Card title={capitalize(pokemon.name)} footer={footer} header={header} className="m-3">
+                {
+                    types.length === 1 ?
+                        <div>
+                            <h2 className='badge ' style={setColor(types[0])}>{types[0]}</h2>
+                        </div> :
+                        <div>
+                            <h6 className='badge' style={setColor(types[0])}>{types[0]}</h6>
+                            <h6 className='badge ml-1' style={setColor(types[1])}>{types[1]}</h6>
+                        </div>
+                }
+            </Card>
+        </div>
+
+    )
 }
 
 
 
 export default RenderCard
-
-/* <div className='cardBody'>
-            <h4 className='cardTitle'>{capitalize(pokemon.name)}</h4>
-            {
-                types.length===1?
-                    <h2 className='badge' style={setColor(types[0])}>{types[0]}</h2>:
-                 <div>
-                    <h6 className='badge' style={setColor(types[0])}>{types[0]}</h6>
-                    <h6 className='badge ml-1' style={setColor(types[1])}>{types[1]}</h6>
-                </div>
-            }
-            
-        </div> */
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
